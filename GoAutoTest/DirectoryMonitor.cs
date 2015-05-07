1695caacb9c879707b347174907d1fa5549db3a2
Rob Archibald <Rob.Archibald@EndFirst.com>
2015-05-06 21:58:02 -0700

    Initial add of GoAutoTest

---
+++ GoAutoTest/DirectoryMonitor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace GoAutoTest
+{
+  public delegate void FileSystemEvent(string path);
+
+  public class DirectoryMonitor
+  { 
+    private readonly FileSystemWatcher watcher =  new FileSystemWatcher(); 
+    private readonly Dictionary<string, DateTime> pendingEvents =  new Dictionary<string, DateTime>(); 
+    private readonly Timer timer; 
+    private bool timerStarted = false;
+
+    public DirectoryMonitor(string dirPath, string filter)
+    {
+      watcher.Path = Path.GetFullPath(dirPath); 
+      watcher.IncludeSubdirectories = true;
+      watcher.Filter = filter;
+      watcher.Created += new FileSystemEventHandler(OnChange); 
+      watcher.Changed += new FileSystemEventHandler(OnChange);  
+      timer = new Timer(OnTimeout, null, Timeout.Infinite, Timeout.Infinite);
+      watcher.EnableRaisingEvents = true;
+      Console.WriteLine("Watching for changes to {0} files in: {1}", filter, Path.GetFullPath(dirPath));
+    }  
+    
+    public event FileSystemEvent Change;
+
+    private void OnChange(object sender, FileSystemEventArgs e) 
+    { 
+      lock (pendingEvents) 
+      { 
+        pendingEvents[e.FullPath] = DateTime.Now;  
+        
+        if (!timerStarted)
+        {
+          timer.Change(100, 100); 
+          timerStarted = true;
+        }  
+      } 
+    }  
+    
+    private void OnTimeout(object state) 
+    { 
+      List<string> paths;  
+      
+      lock (pendingEvents) 
+      { 
+        paths = FindReadyPaths(pendingEvents);  
+        paths.ForEach(i => pendingEvents.Remove(i));  
+
+        if (pendingEvents.Count == 0)
+        {
+          timer.Change(Timeout.Infinite, Timeout.Infinite); 
+          timerStarted = false;
+        } 
+      }  
+      
+      paths.ForEach(FireEvent); 
+    }  
+    
+    private List<string> FindReadyPaths(Dictionary<string, DateTime> events) 
+    { 
+      var results = new List<string>(); 
+      var now = DateTime.Now;  
+      
+      foreach (var item in events)
+      {
+        if (now.Subtract(item.Value).TotalMilliseconds >= 75) 
+          results.Add(item.Key); 
+      }  
+      return results; 
+    }
+
+    private void FireEvent(string path)
+    {
+      FileSystemEvent evt = Change;
+      if (evt != null)
+      {
+        evt(path);
+      }
+    } 
+  } 
+}
---
+++ GoAutoTest/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace GoAutoTest
+{
+  class Program
+  {
+    private static DateTime lastFileChangeTime;
+
+    static void Main(string[] args)
+    {
+      string path = args.Length > 0 ? args[0] : null;
+      while (!Directory.Exists(path) && !File.Exists(path))
+      {
+        Console.WriteLine();
+        Console.Write("Enter directory name to watch: ");
+        path = Console.ReadLine();
+      }
+
+      lastFileChangeTime = DateTime.Now;
+      var watcher = new DirectoryMonitor(path, "*.go");
+      watcher.Change += OnWatcherChange;
+
+      while (true)
+      {
+        System.Threading.Thread.Sleep(100);
+      }
+    }
+
+    static void OnWatcherChange(string path)
+    {
+      RunTests(Path.GetDirectoryName(path));
+    }
+
+    private static void RunTests(string path)
+    {
+      var process = new System.Diagnostics.Process
+      {
+        StartInfo =
+        {
+          FileName = @"C:\Go\bin\go.exe",
+          Arguments = "test -v -short",
+          CreateNoWindow = true,
+          WorkingDirectory = path,
+          UseShellExecute = false,
+          RedirectStandardOutput = true,
+          RedirectStandardError = true
+        }
+      };
+      process.Start();
+      process.WaitForExit();
+      Console.Clear();
+      Console.ForegroundColor = ConsoleColor.White;
+      var hasError = false;
+      string line = null;
+      while (!process.StandardOutput.EndOfStream)
+      {
+        line = process.StandardOutput.ReadLine();
+        if (line.Contains("FAIL"))
+        {
+          Console.ForegroundColor = ConsoleColor.Red;
+          hasError = true;
+        }
+        else if (line.Contains("PASS"))
+        {
+          Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+        else if (line.Contains("SKIP"))
+        {
+          Console.ForegroundColor = ConsoleColor.DarkYellow;
+        }
+        else
+        {
+          Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        if (line.Contains("---"))
+          Console.WriteLine(line);
+      }
+      Console.WriteLine(line);
+      Console.ForegroundColor = ConsoleColor.Red;
+      var errors = process.StandardError.ReadToEnd();
+      Console.WriteLine(errors);
+
+      if (hasError || !string.IsNullOrWhiteSpace(errors))
+      {
+        Console.Beep(660, 200);
+        Console.Beep(440, 200);
+      }
+    }
+  }
+}
